65c0eb0037a667bdca3a521f25fee4b313b0fa3d
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-06-22 06:34:29 -0500

    get call to backend from angualar

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,39 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit{
+
+  private onDestroyRef = inject(DestroyRef);
+  isFetcing = signal(false); // Signal to track if data is being fetched for fallback content display in the template
+  requestError = signal(false); // Signal to track if there was an error during the request
+
   places = signal<Place[] | undefined>(undefined);
+
+  private httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.isFetcing.set(true);
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places',
+      //{observe: 'response',} allows to access the full HTTP response, including headers and status code (other options are 'body' and 'events')
+  ).pipe(
+    map((res)=> res.places) // Extract the 'places' array from the response with operator
+  ).subscribe({
+      next: (places) => {
+        //console.log('Available places:', places);
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetcing.set(false); // Set fetching to false when the request completes to hide the fallback content
+      },
+      error: (error) => {
+        console.error('Error fetching places:', error);
+         this.isFetcing.set(false); 
+        this.requestError.set(true); 
+      }
+    }); 
+
+    this.onDestroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
